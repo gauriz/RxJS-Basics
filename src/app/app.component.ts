a370433823a8f6788d44a2067d77e49d19712474
gauri-focaloid <52951581+gauri-focaloid@users.noreply.github.com>
2020-03-03 12:41:12 +0530

    Operator service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RxJS-Basics';
+
+  ngOnInit() {
+    this.sampleCreationOfObservable();
+    // this.observableLifeCycle();
+    // this.observablesAndFunctions();
+  }
+
+  sampleCreationOfObservable() {
+    const observable = Observable.create((observer: any) => {
+      observer.next('Hello World!');
+    });
+
+    observable.subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  observableLifeCycle() {
+    const observable = Observable.create((observer: any) => {
+      observer.next('Hello World!');
+      // observer.error("Error Passed");
+      observer.complete();
+    });
+
+    observable.subscribe({
+      next(msg) {
+        console.log(msg);
+      },
+      error(err) {
+        console.error(err);
+      },
+      complete() {
+        console.log('Done');
+      }
+    });
+  }
+
+  observablesAndFunctions() {
+    const ret = this.simpleFunction();
+    console.log(ret);
+
+    // Observable subscribes are similar to function calls
+
+    const obs1 = new Observable(observer => {
+      observer.next(100);
+    });
+
+    obs1.subscribe(data => {
+      console.log('From obs1 :', data);
+    });
+
+    // Observable subscribes are similar to function calls,
+    // but can emit multiple values
+
+    const obs2 = new Observable(observer => {
+      observer.next(100);
+      observer.next(200);
+      observer.next(300);
+    });
+
+    obs2.subscribe(data => {
+      console.log('From obs2 :', data);
+    });
+
+    // Observables can return value asynchronously too
+    const obs3 = new Observable(observer => {
+      observer.next(100);
+      setTimeout(() => {
+        observer.next(200); // happens asynchronously
+      }, 2000);
+      observer.next(300);
+    });
+
+    obs3.subscribe(data => {
+      console.log('From obs3 :', data);
+    });
+  }
+
+  simpleFunction() {
+    return 100;
+  }
 }
